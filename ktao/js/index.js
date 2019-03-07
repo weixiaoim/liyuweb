b06cd701978bebaadd066c031fd78d03e33af9a3
sunshile <2311610597@qqcom>
2019-03-07 15:10:56 +0800

    666

--- ktao/js/index.js
+++ ktao/js/index.js
@@ -53,10 +53,28 @@
 	}
 
 	$search.search();
-
 	//分类列表
 	var $categoryDropdown = $('.category .dropdown')
-
+	$categoryDropdown.on('dropdown-show',function(ev){
+		var $elem = $(this);
+		var loadUrl = $elem.data('load');
+		if (!loadUrl) return;
+		var isLoaded = $elem.data('isLoaded');
+		if (isLoaded) return;
+		var $layer = $elem.find('.dropdown-layer');
+		$.getJSON(loadUrl,function(data){
+			var html = '';
+			for(var i = 0;i<data.length;i++){
+				html += '<li><a href="'+data[i].url+'" class="menu-item">'+data[i].name+'</a></li>'
+			}
+			//模拟网路延迟
+			setTimeout(function(){
+				$layer.html(html);
+				$elem.data('isLoaded',true);
+			},1000)
+		
+		})
+	});
 	$categoryDropdown.dropdown({
 		delay:200,
 		js:true,
