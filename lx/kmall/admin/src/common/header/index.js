190cd6c83784fcec3c7b872d9fb22f841fcd9bb8
sunshile <2311610597@qqcom>
2019-04-29 20:48:01 +0800

    0429

--- lx/kmall/admin/src/common/header/index.js
+++ lx/kmall/admin/src/common/header/index.js
@@ -5,15 +5,34 @@ import React,{ Component } from 'react'
 import {
   Layout, Menu, Icon, Dropdown
 } from 'antd';
-import { getUserName } from 'util'
+import { getUserName,request,removeUserName } from 'util'
+import { USER_LOGOUT } from 'api'
 import './index.css'
 
-const { Header, } = Layout;
+const { Header } = Layout;
 
 class AdminHeader extends Component{
+	constructor(props){
+		super(props);
+		this.handleLogout = this.handleLogout.bind(this)
+	}
+	handleLogout(){
+		request({
+			url:USER_LOGOUT
+		})
+		.then(result=>{
+			//退出成功
+			if(result.code == 0){
+				//1.清除本地存储的用户信息
+				removeUserName()
+				//2.页面跳转到登录页面
+				window.location.href = '/login'
+			}
+		})
+	}
 	render(){
 		const menu = (
-	<Menu>
+	<Menu onClick={this.handleLogout}>
 		<Menu.Item key="0">
 			<Icon type="logout" />退出
 		</Menu.Item>
